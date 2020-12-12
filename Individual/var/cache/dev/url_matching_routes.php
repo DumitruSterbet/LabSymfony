b72e8f43f85e7e9615507f352901822381909ee5
DumitruSterbet <74652623+DumitruSterbet@users.noreply.github.com>
2020-12-12 18:14:41 +0200

    Add files via upload

---
+++ Individual/var/cache/dev/url_matching_routes.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/_profiler' => [[['_route' => '_profiler_home', '_controller' => 'web_profiler.controller.profiler::homeAction'], null, null, null, true, false, null]],
+        '/_profiler/search' => [[['_route' => '_profiler_search', '_controller' => 'web_profiler.controller.profiler::searchAction'], null, null, null, false, false, null]],
+        '/_profiler/search_bar' => [[['_route' => '_profiler_search_bar', '_controller' => 'web_profiler.controller.profiler::searchBarAction'], null, null, null, false, false, null]],
+        '/_profiler/phpinfo' => [[['_route' => '_profiler_phpinfo', '_controller' => 'web_profiler.controller.profiler::phpinfoAction'], null, null, null, false, false, null]],
+        '/_profiler/open' => [[['_route' => '_profiler_open_file', '_controller' => 'web_profiler.controller.profiler::openAction'], null, null, null, false, false, null]],
+        '/prod/descriere' => [[['_route' => 'prod_desc_index', '_controller' => 'App\\Controller\\ProdDescController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/prod/descriere/new' => [[['_route' => 'prod_desc_new', '_controller' => 'App\\Controller\\ProdDescController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        '/tara/prod' => [[['_route' => 'tara_prod_index', '_controller' => 'App\\Controller\\TaraProdController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/tara/prod/new' => [[['_route' => 'tara_prod_new', '_controller' => 'App\\Controller\\TaraProdController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_(?'
+                    .'|error/(\\d+)(?:\\.([^/]++))?(*:38)'
+                    .'|wdt/([^/]++)(*:57)'
+                    .'|profiler/([^/]++)(?'
+                        .'|/(?'
+                            .'|search/results(*:102)'
+                            .'|router(*:116)'
+                            .'|exception(?'
+                                .'|(*:136)'
+                                .'|\\.css(*:149)'
+                            .')'
+                        .')'
+                        .'|(*:159)'
+                    .')'
+                .')'
+                .'|/prod/descriere/([^/]++)(?'
+                    .'|(*:196)'
+                    .'|/edit(*:209)'
+                    .'|(*:217)'
+                .')'
+                .'|/tara/prod/([^/]++)(?'
+                    .'|(*:248)'
+                    .'|/edit(*:261)'
+                    .'|(*:269)'
+                .')'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        38 => [[['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null]],
+        57 => [[['_route' => '_wdt', '_controller' => 'web_profiler.controller.profiler::toolbarAction'], ['token'], null, null, false, true, null]],
+        102 => [[['_route' => '_profiler_search_results', '_controller' => 'web_profiler.controller.profiler::searchResultsAction'], ['token'], null, null, false, false, null]],
+        116 => [[['_route' => '_profiler_router', '_controller' => 'web_profiler.controller.router::panelAction'], ['token'], null, null, false, false, null]],
+        136 => [[['_route' => '_profiler_exception', '_controller' => 'web_profiler.controller.exception_panel::body'], ['token'], null, null, false, false, null]],
+        149 => [[['_route' => '_profiler_exception_css', '_controller' => 'web_profiler.controller.exception_panel::stylesheet'], ['token'], null, null, false, false, null]],
+        159 => [[['_route' => '_profiler', '_controller' => 'web_profiler.controller.profiler::panelAction'], ['token'], null, null, false, true, null]],
+        196 => [[['_route' => 'prod_desc_show', '_controller' => 'App\\Controller\\ProdDescController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        209 => [[['_route' => 'prod_desc_edit', '_controller' => 'App\\Controller\\ProdDescController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        217 => [[['_route' => 'prod_desc_delete', '_controller' => 'App\\Controller\\ProdDescController::delete'], ['id'], ['DELETE' => 0], null, false, true, null]],
+        248 => [[['_route' => 'tara_prod_show', '_controller' => 'App\\Controller\\TaraProdController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        261 => [[['_route' => 'tara_prod_edit', '_controller' => 'App\\Controller\\TaraProdController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        269 => [
+            [['_route' => 'tara_prod_delete', '_controller' => 'App\\Controller\\TaraProdController::delete'], ['id'], ['DELETE' => 0], null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
